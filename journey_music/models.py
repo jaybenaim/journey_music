c068a2b0b0b2aafe6ec516ffcca9819af27394c1
jaybenaim <benaimjacob@gmail.com>
2019-08-14 10:26:53 -0400

    initiated models

--- journey_music/models.py
+++ journey_music/models.py
@@ -5,3 +5,22 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from datetime import date 
 from django import forms 
+
+
+
+class Trip(models.Model): 
+    location = models.CharField(max_length=255)
+    weather = models.CharField(max_length=255)
+
+    def __str__(self): 
+        return f'Trip to {self.location}'
+
+
+class Track(models.Model): 
+    trip = models.ForeignKey('Trip', on_delete=models.CASCADE, related_name='tracks')
+    artist = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+
+    def __str__(self): 
+        return f'{self.artist} - {self.track}'
+        
--- journey_music/settings.py
+++ journey_music/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles', 
+    'journey_music', 
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'crowd_surfer.urls'
+ROOT_URLCONF = 'journey_music.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'crowd_surfer.wsgi.application'
+WSGI_APPLICATION = 'journey_music.wsgi.application'
 
 
 # Database
